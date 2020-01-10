7e81f661481925ca86f96d6cb3f6508002f0a2b1
Anibal Chaaim <elder.oliveira@ubicuacloud.com.br>
2020-01-10 18:59:53 -0300

    commitando segunda parte do app

---
+++ public/main.js
@@ -0,0 +1,66 @@
+const form = document.getElementById('vote-form');
+
+// Form Submit event
+form.addEventListener('submit', (e) => {
+    const choice = document.querySelector('input[name=os]:checked').value;
+    const data = { os: choice };
+
+    fetch('http://localhost:3000/poll', {
+        method: 'post',
+        body: JSON.stringify(data),
+        headers: new Headers({
+            'Content-Type': 'application/json'
+        })
+    }).then(res => res.json())
+        .then(data => console.log(data))
+        .catch(err => console.log(err));
+
+    e.preventDefault();
+});
+
+let dataPoints = [
+    { label: 'Trump', y: 0 },
+    { label: 'Bolsonaro', y: 0 },
+    { label: 'Farage', y: 0 },
+    { label: 'Duterte', y: 0 },
+];
+
+const chartContainer = document.querySelector('#chartContainer');
+
+if (chartContainer) {
+    const chart = new CanvasJS.Chart('chartContainer', {
+        animationEnabled: true,
+        theme: 'theme1',
+        title: {
+            text: 'Resultados'
+        },
+        data: [
+            {
+                type: 'column',
+                dataPoints: dataPoints
+            }
+        ]
+    });
+    chart.render();
+
+    // Enable pusher logging - don't include this in production
+    Pusher.logToConsole = true;
+
+    var pusher = new Pusher('df9e73c06d8903bcebd0', {
+      cluster: 'us2',
+      forceTLS: true
+    });
+
+    var channel = pusher.subscribe('os-poll');
+    channel.bind('os-vote', function(data) {
+      dataPoints = dataPoints.map(x=>{
+          if(x.label==data.os){
+              x.y += data.points;
+              return x;
+          }else{
+            return x;  
+          }
+      });
+      chart.render();
+    });
+}
